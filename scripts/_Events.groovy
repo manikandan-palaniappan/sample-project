b87461dbb4e33fdcf79526598ddd647e595cc328
Manikandan <manikandan-project@gmail.com>
2016-06-18 14:11:47 +0530

    Set Info added

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -4,19 +4,20 @@ eventCreateWarStart = { warName, stagingDir ->
         println("BuildNumber " +buildNumber)
         ant.propertyfile(file:"${stagingDir}/WEB-INF/classes/application.properties") {
             entry(key:'build.number', value: buildNumber)
+            entry(key:'app.version', value: buildNumber)
         }
     }
 }
 
-includeTargets << grailsScript("Init")
-
-target(main: "Append a string to the existing version number") {
-    depends(checkVersion, parseArguments)
-
-    def newVersion = metadata.'app.version' + '-' + 123
-    println("newVersion " +newVersion)
-    metadata.'app.version' = newVersion
-    metadata.persist()
-}
-
-setDefaultTarget(main)
+//includeTargets << grailsScript("Init")
+//
+//target(main: "Append a string to the existing version number") {
+//    depends(checkVersion, parseArguments)
+//
+//    def newVersion = metadata.'app.version' + '-' + 123
+//    println("newVersion " +newVersion)
+//    metadata.'app.version' = newVersion
+//    metadata.persist()
+//}
+//
+//setDefaultTarget(main)
