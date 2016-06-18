af82bdd8ab1e4df084418894db49eb05879429e5
Manikandan <manikandan-project@gmail.com>
2016-06-18 14:39:09 +0530

    Set Info added

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -4,9 +4,22 @@ eventCreateWarStart = { warName, stagingDir ->
     if(buildNumber) {
         println("BuildNumber " +buildNumber)
         println("newVersion " +newVersion)
+        
+        metadata.'app.version' = newVersion
+        metadata.'build.number' = buildNumber
+        metadata.persist()
+
         ant.propertyfile(file:"${stagingDir}/WEB-INF/classes/application.properties") {
             entry(key:'build.number', value: buildNumber)
             entry(key:'app.version', value: newVersion)
+        }
+        
+        ant.manifest(file: "${stagingDir}/META-INF/MANIFEST.MF", mode: "update") {
+            attribute(name: "Build-Time", value: new Date())        
+            section(name: "Grails Application") {       
+                attribute(name: "Implementation-Build-Number", value: buildNumber)
+                attribute(name: "Implementation-SCM-Revision", value: scmRevision)
+            }
         }
     }
 }
