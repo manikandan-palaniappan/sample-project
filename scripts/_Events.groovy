e44de3b3dbf4c0e7b390709de8175e5b386def02
Manikandan <manikandan-project@gmail.com>
2016-06-18 13:43:19 +0530

    buildInfo added for creating a automated build number from jenkins

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,6 +1,7 @@
 eventCreateWarStart = { warName, stagingDir ->
     def buildNumber = System.getenv('BUILD_NUMBER')
     if(buildNumber) {
+        println("BuildNumber " +buildNumber)
         ant.propertyfile(file:"${stagingDir}/WEB-INF/classes/application.properties") {
             entry(key:'build.number', value: buildNumber)
         }
