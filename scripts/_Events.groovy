249fc3e441b1d0bde97bc416f1d784d151b68ca2
Manikandan <manikandan-project@gmail.com>
2016-06-18 14:14:23 +0530

    Set Info added

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,7 +1,9 @@
 eventCreateWarStart = { warName, stagingDir ->
     def buildNumber = System.getenv('BUILD_NUMBER')
+    def newVersion = metadata.'app.version' + '-' + 123
     if(buildNumber) {
         println("BuildNumber " +buildNumber)
+        println("newVersion " +newVersion)
         ant.propertyfile(file:"${stagingDir}/WEB-INF/classes/application.properties") {
             entry(key:'build.number', value: buildNumber)
             entry(key:'app.version', value: buildNumber)
