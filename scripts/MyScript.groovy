b38b2217f27c6ab0bde1f7772aaf622a666fb811
Manikandan <manikandan-project@gmail.com>
2016-06-18 19:08:59 +0530

    Tets script added

---
+++ scripts/MyScript.groovy
@@ -0,0 +1,8 @@
+includeTargets << grailsScript("_GrailsInit")
+
+target(myScript: "The description of the script goes here!") {
+    // TODO: Implement script here
+    println "Example Script"
+}
+
+setDefaultTarget(myScript)
